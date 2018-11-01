c35e3ff47b5e145e07b16ca61613c2c754105344
Squinnie <sarahq@kainos.com>
2018-11-01 11:33:37 +0000

    added test data

--- Kainos-map/SQL scripts/dbCreationSetup.sh
+++ Kainos-map/SQL scripts/dbCreationSetup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 read -p "Running this script will delete all non-default data in your database. If you just want to update your database but keep all your data, you may want to run update.sh. Are you sure you want to rebuild your database?" -n 1 -r
 echo    
 if [[ $REPLY =~ ^[Yy]$ ]]; then
--- Kainos-map/SQL scripts/initialise_db.sql
+++ Kainos-map/SQL scripts/initialise_db.sql
@@ -7,7 +7,15 @@ DROP TABLE IF EXISTS Course;
 CREATE TABLE Course (
 	id int AUTO_INCREMENT PRIMARY KEY,
 	title varchar(100) NOT NULL,
-    course_date datetime NOT NULL,
+    course_date date NOT NULL,
     course_location varchar(25),
     info text NOT NULL
 );
+
+INSERT INTO Course (title, course_date, course_location, info) VALUES 
+("Coaching Foundation",	2019-04-13, "Belfast", "For those who coach (Snr Associate +)"),
+("Kimble Basics", 2019-07-30, "Derry-Londonderry", "the basics of kimble"),
+("Performance Testing; Introduction to jmeter", 2019-01-15, "Belfast", "jmeter introduction"),
+("Crucial Conversations", 2018-12-04, "Gdansk", "For people managers - conversations that are crucial"),
+("Kimble Advanced", 2019-08-25, "Derry-Londonderry", "the advanced teachings of kimble"),
+("Kimble Basics", 2019-07-11, "London", "the basics of kimble")
