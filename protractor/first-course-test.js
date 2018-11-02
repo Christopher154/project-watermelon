532f49a5e7b79eb8d7db132cbd83dbf5ef73f598
Matthew Maguire <matthewm@kainos.com>
2018-11-02 10:58:50 +0000

    Renamed test spec file

--- protractor/first-course-test.js
+++
@@ -1,8 +0,0 @@
-describe('KainosMap Courses list', function() {
-  it('should have a title, and table should be populated', function() {
-    browser.get('http://localhost:4200/list');
-
-    expect(element(by.id('title')).getText()).toEqual('Title');
-    expect(element(by.id('courseTitle1')).getText());
-  });
-});
